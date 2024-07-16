8151e96a4e380754397aac1c256ecd2f111ce0a9
Sudha Lohani <sudhalohani1@gmail.com>
2024-07-16 10:40:24 +0530

    Practice-02

--- App.js
+++ App.js
@@ -1,4 +1,32 @@
-{/* <div id = "parent">
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+
+//JSX (transpiled before it reaches the JS) - Parcel - Babel
+
+//JSX => Babel transpiles it to React.createElement => ReactElement- JS Object => HTML Element(render)
+const jsxHeading =  <h1 className="head" tabIndex='5'> React Learning is here using jsx </h1>
+console.log(jsxHeading);
+
+//Functional Component
+const HeadingComponent2 = () => (<h1 className="heading">React Functional Component</h1>
+)
+
+const HeadingComponent = () =>{
+    return(
+   <div id="container">
+    <HeadingComponent2/>,
+    <h1 className="heading">Welcome to  Functional Component </h1>
+    </div>
+    );
+};
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>)
+//root.render(jsxHeading);
+
+  /* <div id = "parent">
     <div id="child">
         <h1>I am h1 tag</h1>
         <h2>I am h2 tag</h2>
@@ -7,26 +35,21 @@
         <h1>I am h1 tag</h1>
         <h2>I am h2 tag</h2>
     </div>
-</div> */}
+</div> */
 
 
 
-const parent = React.createElement("div",{id: "parent"},
-[
-    [React.createElement("div",{id: "child"},
-React.createElement("h1",{},"I m H1 tag"),
-React.createElement("h2",{},"I m H2 tag"),)
-],
-[React.createElement("div",{id: "child2"},
-React.createElement("h1",{},"I m H1 tag"),
-React.createElement("h2",{},"I m H2 tag"),)
-]
-]
-);
+// const parent = React.createElement("div",{id: "parent"},
+// [
+//     [React.createElement("div",{id: "child"},
+// React.createElement("h1",{},"I m Sudha Lohani"),
+// React.createElement("h2",{},"I m H2 tag"),)
+// ],
+// [React.createElement("div",{id: "child2"},
+// React.createElement("h1",{},"I m H1 tag"),
+// React.createElement("h2",{},"I m H2 tag"),)
+// ]
+// ]
+// );
 
 //const heading = React.createElement("h1",{},"Hello world from React!");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-
-
-  
---
+++ README.md
@@ -0,0 +1,24 @@
+# React  
+- NPM:-
+- all the libraries and utilities u need comes from npm 
+- and npm basically manage that for u 
+- when u create basically create react app it automatically has NPM inside it.
+
+# Parcel 
+- Dev Build
+- Local Server
+- HMR =  Hot Module Replacement
+- File Watching Algorithm -  written in C++
+- Caching - Faster Build 
+- Image Optimization
+- Minification of the file 
+-Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Diffrential Bundling - Support older Browsers
+- Diagnostic
+- Errror Handling
+- Https
+- Tree Shaking - remove unused code
+- Diffrent dev and production bundles
